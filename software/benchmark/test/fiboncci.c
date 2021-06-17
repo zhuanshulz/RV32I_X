63b458155461144b884830f1ea010c5fe2323732
zhuanshulz <yangling404690228@qq.com>
2021-06-17 19:49:08 +0800

    update c test program and assembly code generated by gcc

---
+++ software/benchmark/test/fiboncci.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int fib(int m)
+{
+	if (m==1||m==2)
+	return 1;
+	int a=1,b=1,aw=0;
+	while(m>=2)
+	{
+		aw=aw+a;
+		a=b;
+		b=aw;
+		m=m-1;
+	}
+	return aw;
+ } 
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	printf("%d",fib(n));
+	return 0;
+ } 
